568d1ab8ec67e32d8664c012a3e046435023f20b
John Vascsinec <pensteeler@gmail.com>
2017-07-26 18:17:08 -0500

    Initial commit with basic routes defined.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  get '/users/:id' do
+
+  end
+
+  get '/signup' do
+
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  get '/logout' do
+
+  end
+
+end
