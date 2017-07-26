844c39949ed135234d0a094f2078239d18cee7df
John Vascsinec <pensteeler@gmail.com>
2017-07-26 18:15:25 -0500

    Initial commit with basic routes defined.

---
+++ app/controllers/airplanes_controller.rb
@@ -0,0 +1,31 @@
+class AirplanesController < ApplicationController
+
+  get 'airplanes' do
+
+  end
+
+  post '/airplanes' do
+
+  end
+
+  get "/airplanes/:id" do
+
+  end
+
+  post '/airplanes/:id' do
+
+  end
+
+  get "/airplanes/:id/edit" do
+
+  end
+
+  post "/airplanes/:id" do
+
+  end
+
+  get '/airplanes/new' do
+
+  end
+
+end
